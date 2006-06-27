8d1b72de21d468a1a9f38105e13e9ce84e85447e
jdiamond <jdiamond@741b5af6-0615-0410-9410-c31befd42fc1>
2006-06-27 02:13:10 +0000

    Now including Smarty in the solidworks directory

---
+++ manager/pages/ViewOrderPage.class.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * ViewOrderPage.class.php
+ *
+ * This file contains the definition for the View Order Page class
+ *
+ * @package Pages
+ * @author John Diamond <jdiamond@solid-state.org>
+ * @copyright John Diamond <jdiamond@solid-state.org>
+ * @license http://www.opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+// Include the parent class
+require_once $base_path . "solidworks/Page.class.php";
+
+// OrderDBO class
+require_once $base_path . "DBO/OrderDBO.class.php";
+
+/**
+ * ViewOrderPage
+ *
+ * Display an order.
+ *
+ * @package Pages
+ * @author John Diamond <jdiamond@solid-state.org>
+ */
+class ViewOrderPage extends Page
+{
+  /**
+   * @var OrderDBO The order
+   */
+  var $orderDBO = null;
+
+  /**
+   * Action
+   *
+   * Actions handled by this page:
+   *   browse_accounts_action (form)
+   *
+   * @param string $action_name Action
+   */
+  function action( $action_name )
+  {
+    switch( $action_name )
+      {
+      case "order":
+	if( isset( $this->session['order']['execute'] ) )
+	  {
+	    $this->execute();
+	  }
+	elseif( isset( $this->session['order']['save'] ) )
+	  {
+	    $this->save();
+	  }
+	elseif( isset( $this->session['order']['delete'] ) )
+	  {
+	    $this->delete();
+	  }
+	break;
+	
+      default:
+	// No matching action, refer to base class
+	parent::action( $action_name );
+      }
+  }
+
+  /**
+   * Delete Order
+   */
+  function delete()
+  {
+    if( !delete_OrderDBO( $this->orderDBO ) )
+      {
+	fatal_error( "ViewOrderPage::delete()",
+		     "Could not delete Order.  ID = " . $this->orderDBO->getID() );
+      }
+
+    // Success
+    $this->setMessage( array( "type" => "ORDER_DELETED",
+			      "args" => array( $this->orderDBO->getID() ) ) );
+    $this->goto( "pending_orders" );
+  }
+
+  /**
+   * Execute Order
+   */
+  function execute()
+  {
+    // Save any changes made to the order
+    $this->saveChanges();
+
+    // Redirect to the execute order page
+    $this->goto( "execute_order", null, sprintf( "id=%d", $this->orderDBO->getID() ) );
+  }
+
+  /**
+   * Initialize the View Order Page
+   */
+  function init()
+  {
+    $this->orderDBO =& $this->session['orderdbo'];
+    if( isset( $_GET['id'] ) )
+      {
+	// Retrieve the Order from the database
+	$this->orderDBO = load_OrderDBO( intval( $_GET['id'] ) );
+      }
+
+    if( !isset( $this->orderDBO ) )
+      {
+	// Could not find Server
+	fatal_error( "ViewOrderPage::init()", 
+		     "Could not load Order.  ID = " . $_GET['id'] );
+      }
+
+    // Store item id's in the session to be used for the "Accept" checkboxes
+    foreach( $this->orderDBO->getItems() as $itemDBO )
+      {
+	$this->session['itemids'][] = $itemDBO->getOrderItemID();
+      }
+
+    // Set Nav vars
+    $this->setNavVar( "order_id", $this->orderDBO->getID() );
+  }
+
+  /**
+   * Populate the Order Item's Table
+   *
+   * @return array An array of all OrderItemDBO's for this Order
+   */
+  function populateItemTable()
+  {
+    return $this->orderDBO->getItems();
+  }
+  
+  /**
+   * Save Order
+   */
+  function save()
+  {
+    if( !$this->saveChanges() )
+      {
+	// DB Error
+	fatal_error( "ViewOrderPage::save()",
+		     "Could not update Order. ID = " . $this->orderDBO->getID() );
+      }
+
+    $this->setMessage( array( "type" => "ORDER_SAVED" ) );
+  }
+
+  /**
+   * Save Changes
+   *
+   * Write any changes made to the order to the database
+   *
+   * @return boolean True for success
+   */
+  function saveChanges()
+  {
+    // Update OrderDBO
+    $this->orderDBO->setContactName( $this->session['order']['contactname'] );
+    $this->orderDBO->setContactEmail( $this->session['order']['contactemail'] );
+    $this->orderDBO->setAddress1( $this->session['order']['address1'] );
+    $this->orderDBO->setAddress2( $this->session['order']['address2'] );
+    $this->orderDBO->setCity( $this->session['order']['city'] );
+    $this->orderDBO->setState( $this->session['order']['state'] );
+    $this->orderDBO->setCountry( $this->session['order']['country'] );
+    $this->orderDBO->setPostalCode( $this->session['order']['postalcode'] );
+    $this->orderDBO->setPhone( $this->session['order']['phone'] );
+    $this->orderDBO->setMobilePhone( $this->session['order']['mobilephone'] );
+    $this->orderDBO->setFax( $this->session['order']['fax'] );
+
+    foreach( $this->orderDBO->getItems() as $itemDBO )
+      {
+	if( in_array( $itemDBO->getOrderItemID(), $this->session['order']['accepted'] ) )
+	  {
+	    $this->orderDBO->acceptItem( $itemDBO->getOrderItemID() );
+	  }
+	else
+	  {
+	    $this->orderDBO->rejectItem( $itemDBO->getOrderItemID() );
+	  }
+      }
+
+    // Save changes to database
+    return update_OrderDBO( $this->orderDBO );
+  }
+}
+
+?>
---
+++ order/pages/DomainContactPage.class.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * DomainContactPage.class.php
+ *
+ * This file contains the definition for the DomainContactPage class
+ *
+ * @package Pages
+ * @author John Diamond <jdiamond@solid-state.org>
+ * @copyright John Diamond <jdiamond@solid-state.org>
+ * @license http://www.opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+// Include the parent class
+require_once $base_path . "solidworks/Page.class.php";
+
+// Order DBO
+require_once $base_path . "DBO/OrderDBO.class.php";
+
+/**
+ * DomainContactPage
+ *
+ * @package Pages
+ * @author John Diamond <jdiamond@solid-state.org>
+ */
+class DomainContactPage extends Page
+{
+  /**
+   * @var OrderDomainItem The domain we are collecting contact information for
+   */
+  var $domainItem;
+
+  /**
+   * @var array Array of Order Domain Item's that still need contact info
+   */
+  var $domainsNeedContact = array();
+
+  /**
+   * Action
+   *
+   * Actions handled by this page:
+   *
+   * @param string $action_name Action
+   */
+  function action( $action_name )
+  {
+    switch( $action_name )
+      {
+      case "domain_contact":
+	if( isset( $this->session['domain_contact']['startover'] ) )
+	  {
+	    $this->newOrder();
+	  }
+	elseif( isset( $this->session['domain_contact']['back'] ) )
+	  {
+	    $this->goto( "customer" );
+	  }
+	elseif( isset( $this->session['domain_contact']['continue'] ) )
+	  {
+	    $this->processContact();
+	  }
+	break;
+
+      default:
+	// No matching action, refer to base class
+	parent::action( $action_name );
+      }
+  }
+
+  /**
+   * Done Processing Domains
+   */
+  function done()
+  {
+    $this->goto( "review" );
+  }
+
+  /**
+   * Build an array of Domain Items that need contact info
+   *
+   * @return array Domain items
+   */
+  function getDomainsNeedingContacts()
+  {
+    // Build a list of domains that need contact information
+    $domains = array();
+    foreach( $this->session['order']->getDomainItems() as $domainItem )
+      {
+	if( !$domainItem->hasContactInformation() )
+	  {
+	    // Add this domain to the list of those who need contact info
+	    $domains[] = $domainItem;
+	  }
+      }
+
+    return $domains;
+  }
+
+  /**
+   * Initialize Customer Page
+   */
+  function init()
+  {
+    if( !isset( $_SESSION['order'] ) || $_SESSION['order']->isEmpty() )
+      {
+	// No order, or order is empty.  Go back the the cart and start a new one
+	$this->goto( "cart" );
+      }
+
+    // Give access to the template
+    $this->session['order'] =& $_SESSION['order'];
+
+    if( null == ($domainItems = $this->session['order']->getDomainItems()) )
+      {
+	// No domains, skip this step
+	$this->done();
+      }
+
+    $this->domainsNeedContact = $this->getDomainsNeedingContacts();
+    if( count( $this->domainsNeedContact ) < 1 )
+      {
+	// No domains need contact information
+	$this->done();
+      }
+
+    // We will collect contact information for the item on the top of the list
+    $this->domainItem = $this->domainsNeedContact[0];
+
+    $this->smarty->assign( "fqdn", $this->domainItem->getfullDomainName() );
+  }
+
+  /**
+   * Start New Order
+   */
+  function newOrder()
+  {
+    // Start a new order
+    unset( $_SESSION['order'] );
+    $this->goto( "cart" );
+  }
+
+  /**
+   * Populate Domains Table
+   *
+   * @return array Array of OrderDomainDBO's that still need contact info
+   */
+  function populateDomainTable()
+  {
+    // Return a list of domain that need contact information, except for the one
+    // that is being collected right now.
+    $domains = $this->domainsNeedContact;
+    array_shift( $domains );
+    return $domains;
+  }
+
+  /**
+   * Process Domain Contact
+   */
+  function processContact()
+  {
+    $adminContact =
+      array( "bussinessname" => $this->session['domain_contact']['abusinessname'],
+	     "contactname" => $this->session['domain_contact']['acontactname'],
+	     "contactemail" => $this->session['domain_contact']['acontactemail'],
+	     "address1" => $this->session['domain_contact']['aaddress1'],
+	     "address2" => $this->session['domain_contact']['aaddress2'],
+	     "address3" => $this->session['domain_contact']['aaddress3'],
+	     "country" => $this->session['domain_contact']['acountry'],
+	     "city" => $this->session['domain_contact']['acity'],
+	     "state" => $this->session['domain_contact']['astate'],
+	     "postalcode" => $this->session['domain_contact']['apostalcode'],
+	     "phone" => $this->session['domain_contact']['aphone'],
+	     "fax" => $this->session['domain_contact']['afax'] );
+
+    $billingContact =
+      array( "bussinessname" => $this->session['domain_contact']['bbusinessname'],
+	     "contactname" => $this->session['domain_contact']['bcontactname'],
+	     "contactemail" => $this->session['domain_contact']['bcontactemail'],
+	     "address1" => $this->session['domain_contact']['baddress1'],
+	     "address2" => $this->session['domain_contact']['baddress2'],
+	     "address3" => $this->session['domain_contact']['baddress3'],
+	     "country" => $this->session['domain_contact']['bcountry'],
+	     "city" => $this->session['domain_contact']['bcity'],
+	     "state" => $this->session['domain_contact']['bstate'],
+	     "postalcode" => $this->session['domain_contact']['bpostalcode'],
+	     "phone" => $this->session['domain_contact']['bphone'],
+	     "fax" => $this->session['domain_contact']['bfax'] );
+
+    $techContact =
+      array( "bussinessname" => $this->session['domain_contact']['tbusinessname'],
+	     "contactname" => $this->session['domain_contact']['tcontactname'],
+	     "contactemail" => $this->session['domain_contact']['tcontactemail'],
+	     "address1" => $this->session['domain_contact']['taddress1'],
+	     "address2" => $this->session['domain_contact']['taddress2'],
+	     "address3" => $this->session['domain_contact']['taddress3'],
+	     "country" => $this->session['domain_contact']['tcountry'],
+	     "city" => $this->session['domain_contact']['tcity'],
+	     "state" => $this->session['domain_contact']['tstate'],
+	     "postalcode" => $this->session['domain_contact']['tpostalcode'],
+	     "phone" => $this->session['domain_contact']['tphone'],
+	     "fax" => $this->session['domain_contact']['tfax'] );
+
+    // Copy the form contents into the domain we are collecting for
+    $this->session['order']->setDomainContact( $this->domainItem->getOrderItemID(),
+					       $adminContact,
+					       $billingContact,
+					       $techContact );
+
+    // And all the other domains selected from the table
+    if( isset( $this->session['domain_contact']['domains'] ) )
+      {
+	foreach( $this->session['domain_contact']['domains'] as $itemID )
+	  {
+	    $this->session['order']->setDomainContact( $itemID,
+						       $adminContact,
+						       $billingContact,
+						       $techContact );
+	  }
+      }
+
+    if( count( $this->getDomainsNeedingContacts() ) < 1 )
+      {
+	// No more domains to collect contact info for
+	$this->done();
+      }
+  }
+}
