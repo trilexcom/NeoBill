d70355a17694e6d09d8dab8b883149326c06d88f
hitchou <hitchou@741b5af6-0615-0410-9410-c31befd42fc1>
2007-11-23 11:14:02 +0000

    Fixed Config.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -25,7 +25,7 @@
 **/
 
 $this_pathinfo  = pathinfo( __FILE__ );
-define( "BASE_PATH", ereg_replace("config", "", $this_pathinfo['dirname']) . "/" );
+define( "BASE_PATH", ereg_replace("config", "", $this_pathinfo['dirname']));
 
 /** 
  * ----------------------------------------------------------------------
