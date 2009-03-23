e93f41dac569c42378c87beec692726f3d03408b
yveskreis <yveskreis@741b5af6-0615-0410-9410-c31befd42fc1>
2009-03-23 17:24:51 +0000

    Installer for v0.5 Beta

---
+++ install/language/index.php
@@ -0,0 +1,8 @@
+<?php
+  $folder = 'language';
+  if (strrpos($_SERVER['PHP_SELF'], '/' . $folder . '/index.php') === (strlen($_SERVER['PHP_SELF']) - strlen('/' . $folder . '/index.php'))) {
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/' . substr($_SERVER['PHP_SELF'], 1, strrpos($_SERVER['PHP_SELF'], '/' . $folder . '/index.php')));
+  } else {
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/');
+  }
+?>
