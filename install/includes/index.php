999c01b720a2850ed6c0d340ef8941560f843640
yveskreis <yveskreis@741b5af6-0615-0410-9410-c31befd42fc1>
2009-03-26 20:37:48 +0000

    Installer for v0.5 Beta

--- install/includes/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $folder = 'includes';
-  if (strrpos($_SERVER['PHP_SELF'], '/' . $folder . '/index.php') === (strlen($_SERVER['PHP_SELF']) - strlen('/' . $folder . '/index.php'))) {
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/' . substr($_SERVER['PHP_SELF'], 1, strrpos($_SERVER['PHP_SELF'], '/' . $folder . '/index.php')));
-  } else {
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/');
-  }
-?>
